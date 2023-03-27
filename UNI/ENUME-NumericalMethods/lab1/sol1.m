ff4f3d6abfd69a55f0b6ab2475f56933f9e0b5b9
Christopher Watras <chris.watras@gmail.com>
2023-03-27 19:04:21 +0200

    sol made to match the raport solution

--- UNI/ENUME-NumericalMethods/lab1/sol1.m
+++ UNI/ENUME-NumericalMethods/lab1/sol1.m
@@ -3,7 +3,7 @@ clc, clearvars, close all
 
 % define domain and function
 x = linspace(0,3,100);
-y = @(x) cos(x)./x.^3 + x.^2;
+y = @(x) cos(x)./x.^3 - x.^2;
 % define erronous domain
 esim = 1.0e-8;
 x_eps = x.*(1+esim);
@@ -18,14 +18,14 @@ tx = 1/esim * abs_error;
 % calculate T analiticaly (formula calculated in the report)
 T1 = -sin(x)./(x.^2);
 T2 = -3*cos(x)./(x.^3);
-T3 = -cos(x)./(x.^3);
-T4 = x.^2;
+T3 = x.^2;
 
-analitical = (abs(T1) + abs(T2) + abs(T3) + abs(T4))./ydot;
+analitical = (abs(T1) + abs(T2) + abs(T3))./ydot;
 tx_analitical = analitical;
 
 % plot results to validate both methods result in simmilar results
 semilogy(x,tx, '*');
 hold on 
 semilogy(x,tx_analitical, '*');
-title("Plot of T(x)"), xlabel("x"), ylabel("y = T(x)"), legend("Numerical", "Analitical")
+title("Plot of T(x)"), xlabel("x"), ylabel("y = T(x)"),
+legend("Numerical", "Analitical")
